6f03c6a6b81e5c1d5563286d3efa5405036b8544
Platos-Poet <82967393+Platos-Poet@users.noreply.github.com>
2023-04-25 13:13:49 -0500

    pain

--- Web-App/api.py
+++ Web-App/api.py
@@ -11,11 +11,11 @@ def get_weather(city):
     url = f'https://api.openweathermap.org/data/2.5/weather?q=Ruston&appid=b2f82104ab9851dcf5b08eb56d3ede6a'
     response = requests.get(url)
     if response.status_code == 200:
-        weather_data = response.json()
-        temperature = round(weather_data['main']['temp'] - 273.15, 2) # convert temperature from Kelvin to Celsius
-        humidity = weather_data['main']['humidity']
-        wind_speed = weather_data['wind']['speed']
-        weather_description = weather_data['weather'][0]['description']
+        weatherData = response.json()
+        temperature = round(weatherData['main']['temp'] - 273.15, 2) # convert temperature from Kelvin to Celsius
+        humidity = weatherData['main']['humidity']
+        wind_speed = weatherData['wind']['speed']
+        weather_description = weatherData['weather'][0]['description']
         result = {
             'city': city,
             'temperature': temperature,
@@ -29,3 +29,4 @@ def get_weather(city):
 
 if __name__ == '__main__':
     app.run()
+
